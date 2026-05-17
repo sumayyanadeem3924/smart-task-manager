71cbe3e55cdb5fc4a3f32fbdee23486204295df1
Sumayya Nadeem <sumayyanadeem3924@gmail.com>
2026-05-17 09:41:06 +0500

    Fixed Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Smart Task Manager API Running");
+});
 
 app.use("/api/tasks", taskRoutes);
 app.use("/api/auth", authRoutes);

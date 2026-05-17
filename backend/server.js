161744bcf72451bb2b98555193f632a91e0c3e27
Sumayya Nadeem <sumayyanadeem3924@gmail.com>
2026-05-17 14:42:22 +0500

    Minimal backend runtime test

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,9 @@
-require("dotenv").config();
-
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
-
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Smart Task Manager API Running");
 });
-
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/tasks", require("./routes/taskRoutes"));
 
 module.exports = app;

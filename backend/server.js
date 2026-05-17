d83368b66d7aec7cfec2f6ae16000ec4469f942b
Sumayya Nadeem <sumayyanadeem3924@gmail.com>
2026-05-17 09:11:46 +0500

    Prepared backend for Vercel

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,9 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 sequelize.sync().then(() => {
+  const PORT = process.env.PORT || 5000;
+
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  console.log(`Server running on port ${PORT}`);
+});;
 });
